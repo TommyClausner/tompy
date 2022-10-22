3a1410dc42aee113c7b96902e548337640d7ba0f
TommyClausner <tommy.clausner@gmail.com>
2022-10-22 19:52:54 +0200

    test custom pdf

--- tompy/tests/test_core.py
+++ tompy/tests/test_core.py
@@ -3,23 +3,6 @@ from numpy.testing import assert_array_equal
 import pytest
 
 from tompy.core import argsort_like, rankdata
-from tompy.stats import CustomPDF
-
-
-def test_custompdf():
-    n = 10000
-    np.random.seed(42)
-    arbitrary_dist = np.random.randn(n)/2 + (np.random.randn(n) + 1)/2
-    pdf = CustomPDF(arbitrary_dist)
-    np.random.seed(42)
-    vals0 = np.sort(pdf(n))
-    np.random.seed(42)
-    vals1 = np.sort(pdf.draw(n))
-
-    assert_array_equal(vals0, vals1)
-
-    spread = np.std(arbitrary_dist)
-    assert (np.std((np.sort(arbitrary_dist) - vals0))/spread) < 0.05  # 95% acc
 
 
 def test_argsort_like():
